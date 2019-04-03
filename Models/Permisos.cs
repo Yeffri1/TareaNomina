d40197dee61d6316a02378a8627c55ef4bf95ac9
DESKTOP-TV3HM18\Yeffri Duarte <20163738@itla.edu.do>
2019-04-02 21:59:26 -0500

    modelo

---
+++ Models/Permisos.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace NominaTarea.Models
+{
+    public class Permisos
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int Id { get; set; }
+        public int IdEmpleado { get; set; }
+
+        [Required]
+        [DataType(DataType.DateTime)]
+        public DateTime Desde { get; set; }
+
+        [Required]
+        [DataType(DataType.DateTime)]
+        public DateTime Hasta { get; set; }
+
+        [Required]
+        [StringLength(500, ErrorMessage = "No puede tener mas de 500 caracteres.")]
+        public string Comentario { get; set; }
+
+        [ForeignKey("IdEmpleado")]
+        public virtual Empleado Empleado { get; set; }
+    }
+}
---
+++ Models/Salida.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace NominaTarea.Models
+{
+    public class Salida
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int Id { get; set; }
+
+        public int IdEmpleado { get; set; }
+
+        [Required]
+        public DateTime FechaSalida { get; set; }
+
+        [Required]
+        public string TipoSalida { get; set; }
+
+
+        [Required]
+        public string Motivo { get; set; }
+
+        [ForeignKey("IdEmpleado")]
+        public virtual Empleado Empleado { get; set; }
+    }
+}
